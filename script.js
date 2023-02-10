ddea5093942f321c4a70262a92b06eb817144ad4
LukeBeaumont <lbeaumont50@msn.com>
2023-02-10 13:44:51 +0000

    Add color buttons to html

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", createBoard(15));
+let color = "black";
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    createBoard(16);
+
+    let sizeButton = document.querySelector(".size-button");
+    sizeButton.addEventListener("click", ()=> {
+    
+    let size = selectSize();
+    createBoard(size);
+  })
+})
 
 function createBoard (size) {
     let grid = document.querySelector(".grid");
@@ -12,7 +23,20 @@ function createBoard (size) {
     let div = document.createElement("div");
 
     grid.insertAdjacentElement("beforeend", div);
-    
+   // div.addEventListener("mouseover", colorDiv());
   }
 }
 
+function selectSize () {
+    let input = prompt("What size grid would you like?");
+    let info = document.querySelector(".info");
+    if (input == "") {
+        info.textContent = "Please input number";
+    }
+    else if (input <= 0 || input > 100) {
+        info.textContent = "Number must be between 1 and 100";
+    }
+    else {info.textContent = `Grid size: ${input}`;
+    return input;
+    }
+}
